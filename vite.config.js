ada9cca2fa1e97e8e81c396bab0fcc6f20a2df89
hayeon <qdt0650@naver.com>
2025-02-13 15:18:13 +0900

    chore: package-lock.json 재설치 및 vite.config 생성

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+   server: {
+      port: 5000,
+      open: true,
+   },
+   base: '/interactive_publishing/',
+   build: {
+      outDir: 'dist',
+   },
+})
